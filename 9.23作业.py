420b53140e47f71e71bc7a376ee3975938f51c0c
蒋逸仙 <110285062+JiangYiXian@users.noreply.github.com>
2022-11-06 14:37:50 +0800

    Add files via upload

---
+++ 9.23作业.py
@@ -0,0 +1,52 @@
+# times=0
+# count=3
+# while times<=3:
+#     age=int(input('猜的年龄'))
+#     if age==25:
+#         print('猜对了')
+#         break
+#         pass
+#     elif age>25:
+#         print('大了')
+#         pass
+#     else:
+#         print('猜小了')
+#         pass
+#     times+=1
+#     if times==3:
+#         choose=input('是否还要继续，，，YorN')
+#         if choose=='Y' or choose=='y':
+#             times=0
+#             pass
+#         elif choose=='N' or choose=='N':
+#             times==4
+#             pass
+#         else:
+#             print('请配合')
+#             pass
+#     pass
+
+
+
+
+# height=1.72
+# weight=100
+#
+# bmi=weight/(height**2)
+#
+# if bmi<18.5:
+#    print('什么瘦猴')
+#    pass
+# elif 18.5<=bmi<=25:
+#     print('多吃点')
+#     pass
+# elif 25<=bmi<=28:
+#        print('十分健康')
+#        pass
+# elif 28<=bmi<=32:
+#        print('该减肥了')
+#        pass
+# else:
+#        print('!!!')
+#        pass
+# pass
